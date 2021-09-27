ad1d0a30d22f93fc9973c380859265240d0d97ba
suyeon-hong <bellahong95@gmail.com>
2021-09-27 16:26:43 +0900

    상세페이지 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const article = document.querySelectorAll("#section article");
+const aside = document.querySelector("#aside");
+const btn = aside.querySelector(".btn");
 
 for (let el of article){
     el.addEventListener("mouseenter", ()=>{
@@ -7,4 +9,22 @@ for (let el of article){
     el.addEventListener("mouseleave", ()=>{
         el.querySelector("video").pause();
     });
+    el.addEventListener("click", e=>{
+        e.preventDefault();
+        let tit = e.currentTarget.querySelector("h2").innerText;
+        let txt = e.currentTarget.querySelector("p").innerText;
+        let vidSrc = e.currentTarget.querySelector("video").getAttribute("src");
+
+        aside.classList.add("on");
+        aside.querySelector("h1").innerText = tit;
+        aside.querySelector("p").innerText = txt;
+        aside.querySelector("video").setAttribute("src", vidSrc);
+        aside.querySelector("video").play();
+    })
+    btn.addEventListener("click", e=>{
+        e.preventDefault();
+
+        aside.classList.remove("on");
+        aside.querySelector("video").pause();
+    })
 }
